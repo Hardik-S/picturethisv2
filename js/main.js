af3f899e932b931ebec8334c194ce5351342d6e0
Hardik Shrestha <hardik.ho@gmail.com>
2024-11-25 13:06:38 -0500

    Add files 05 - mvp 1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,2 @@
-
+console.log('Script loaded and running');
 document.documentElement.style.scrollBehavior = 'smooth';
-document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
-
-    // Function to generate a random even number between 2 and 104
-    function getRandomEvenNumber() {
-        return Math.floor((Math.random() * 51) + 1) * 2;
-    }
-
-    cards.forEach((card, index) => {
-        const img = card.querySelector('img');
-        img.src = `/../images/${getRandomEvenNumber()}.png`;
-        console.log(img.src);
-        // Add click event listener for flipping the cards
-        card.addEventListener('click', () => {
-            card.querySelector('.card-inner').classList.toggle('is-flipped');
-        });
-    });
-});
