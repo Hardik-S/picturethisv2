19852b21f53bdbee1ec3dd6aab8e1d4f2e2d2636
Hardik Shrestha <hardik.ho@gmail.com>
2024-11-25 01:32:34 -0500

    Add files 04 - panels pls

--- js/main.js
+++ js/main.js
@@ -1,26 +1,20 @@
 
 document.documentElement.style.scrollBehavior = 'smooth';
-const cart = [];
-document.querySelector('#addToCartButton').addEventListener('click', () => {
-    // Create alert element
-    const alert = document.createElement('div');
-    alert.textContent = 'Added to Cart!';
-    alert.style.position = 'fixed';
-    alert.style.top = '20px';
-    alert.style.right = '20px';
-    alert.style.padding = '10px 20px';
-    alert.style.backgroundColor = '#5cb85c';
-    alert.style.color = '#fff';
-    alert.style.borderRadius = '5px';
-    alert.style.boxShadow = '0 0 10px rgba(0,0,0,0.1)';
-    alert.style.zIndex = '1000';
-    document.body.appendChild(alert);
+document.addEventListener('DOMContentLoaded', function () {
+    const cards = document.querySelectorAll('.card');
 
-    // Remove the alert after 3 seconds
-    setTimeout(() => {
-        alert.remove();
-    }, 3000);
-    cart.push('item name');
-    console.log(cart);
-    showCart();
+    // Function to generate a random even number between 2 and 104
+    function getRandomEvenNumber() {
+        return Math.floor((Math.random() * 51) + 1) * 2;
+    }
+
+    cards.forEach((card, index) => {
+        const img = card.querySelector('img');
+        img.src = `/../images/${getRandomEvenNumber()}.png`;
+        console.log(img.src);
+        // Add click event listener for flipping the cards
+        card.addEventListener('click', () => {
+            card.querySelector('.card-inner').classList.toggle('is-flipped');
+        });
+    });
 });
